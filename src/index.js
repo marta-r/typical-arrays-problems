11616aa649aae2c120a1832dd37eb79b0f2c3678
Marta Rudkouskaya <lvlartik@gmail.com>
2020-03-02 17:13:49 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    if (!arguments[0] || array.length === 0) {
+        return 0;
+    } else {
+        array.sort((a, b) => a - b);
+        return array[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!arguments[0] || array.length === 0) {
+        return 0;
+    } else {
+        array.sort((a, b) => a - b);
+        return array[array.length - 1];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!arguments[0] || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce((sum, current) => sum + current, 0) / array.length;
+    }
+};
